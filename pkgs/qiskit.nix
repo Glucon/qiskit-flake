4ba519bd13f73176a47ecc2d0cca675e235f0019
Glucon <ombindraindode@proton.me>
2025-04-02 13:14:52 +0800

    chore(qiskit): update to 2.0.0

--- pkgs/qiskit.nix
+++ pkgs/qiskit.nix
@@ -16,12 +16,12 @@
 }:
 buildPythonPackage rec {
   pname = "qiskit";
-  version = "1.4.2";
+  version = "2.0.0";
   src = fetchFromGitHub {
     owner = "qiskit";
     repo = "Qiskit";
     rev = version;
-    hash = "sha256-R9baj15eHyAc5rNx7fixTJrL8mhaDJDceE/00n55heU=";
+    hash = "sha256-RRewZ9ET1Nbnmg7vi2ij5uulatwpDmaTOJaFmo7ZyN4=";
   };
   propagatedBuildInputs = [
     rustworkx
@@ -37,7 +37,7 @@ buildPythonPackage rec {
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit pname version src;
-    hash = "sha256-BEkiGuVU00CxTdTm1p0cjtX63xczrSiJ+1LsF4vR6Ho=";
+    hash = "sha256-axt873O4vukF25MHTITBo+eAWsOZRuV6PS/EiBoK5Xs=";
   };
   doCheck = false;
 }
