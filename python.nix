0ea2c56d13efc7ab3c466ac88716c2531c964f76
Glucon <ombindraindode@proton.me>
2025-04-10 14:24:53 +0800

    chore: use self and super

--- python.nix
+++ python.nix
@@ -1,16 +1,15 @@
 { pkgs, pythonVersion }:
-
 let
   fmt_8 = pkgs.callPackage ./pkgs/fmt_8.nix { };
   spdlog_1_9_2 = pkgs.callPackage ./pkgs/spdlog_1_9_2 { inherit fmt_8; };
   nlohmann_json_3_1_1 = pkgs.callPackage ./pkgs/nlohmann_json_3_1_1.nix { };
   pythonEnv = pythonVersion.override {
-    packageOverrides = pyfinal: pyprev: {
-      qiskit = pyfinal.callPackage ./pkgs/qiskit.nix { };
-      qiskit-aer = pyfinal.callPackage ./pkgs/qiskit-aer.nix {
+    packageOverrides = self: super: {
+      qiskit = self.callPackage ./pkgs/qiskit.nix { };
+      qiskit-aer = self.callPackage ./pkgs/qiskit-aer.nix {
         inherit spdlog_1_9_2 nlohmann_json_3_1_1;
       };
-      qiskit-machine-learning = pyfinal.callPackage ./pkgs/qiskit-machine-learning.nix { };
+      qiskit-machine-learning = self.callPackage ./pkgs/qiskit-machine-learning.nix { };
     };
   };
 in
